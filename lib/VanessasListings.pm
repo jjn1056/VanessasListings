c03b5c26b198cd747bdde2894bc68410a780e029
John Napiorkowski <jjn1056@yahoo.com>
2011-04-06 19:06:21 -0400

    initial commit

---
+++ lib/VanessasListings.pm
@@ -0,0 +1,38 @@
+package VanessasListings;
+
+use 5.008008;
+use strict;
+use warnings FATAL =>'all';
+
+our $VERSION = '0.01';
+
+1;
+
+=head1 NAME
+
+VanessasListings - My Brand New Module
+
+=head1 SYNOPSIS
+
+  use VanessasListings;
+
+=head1 DESCRIPTION
+
+VanessasListings is ...
+
+=head1 AUTHOR
+
+John Napiorkowski L<email:jjnapiork@cpan.org>
+
+=head1 SEE ALSO
+
+    TBD
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2011, John Napiorkowski L<email:jjnapiork@cpan.org> 
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
